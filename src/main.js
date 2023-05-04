f1ba4283013d43a74eb31e4b02e2fdd9a8a02ce2
festsnusa <lukatimof@mail.ru>
2023-05-04 19:52:31 +0700

    added persistedState

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import UUID from 'vue-uuid'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 import './assets/main.scss'
 import { Form as VeeForm, Field as VeeField } from 'vee-validate'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(UUID)
 app.component("VeeForm", VeeForm)
 app.component("VeeField", VeeField)
